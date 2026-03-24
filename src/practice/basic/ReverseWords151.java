87134f121f3cbd7f2d5abb8c24b443e1a246ca4c
Ankit Konchady <ankit.konchady@neebal.com>
2026-03-25 01:15:09 +0530

    [ADD]--Reverse Words LC 151

---
+++ src/practice/basic/ReverseWords151.java
@@ -0,0 +1,39 @@
+package practice.basic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseWords151 {
+
+    public static void main(String[] args) {
+        String s = "a good   example";
+        System.out.println(reverseWords(s));
+    }
+
+    public static String reverseWords(String s) {
+        List<String> wordList = new ArrayList<>();
+        int length = s.length();
+
+        for (int index = 0; index < length; ) {
+            while (index < length && s.charAt(index) == ' ') {
+                index++;
+            }
+
+            if (index < length) {
+                StringBuilder currentWord = new StringBuilder();
+                int wordStartIndex = index;
+                while (wordStartIndex < length && s.charAt(wordStartIndex) != ' ') {
+                    currentWord.append(s.charAt(wordStartIndex));
+                    wordStartIndex++;
+                }
+                wordList.add(currentWord.toString());
+                index = wordStartIndex;
+            }
+        }
+        Collections.reverse(wordList);
+
+        return String.join(" ", wordList);
+    }
+}
