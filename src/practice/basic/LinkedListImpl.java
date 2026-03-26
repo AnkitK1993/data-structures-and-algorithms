a8ce3d5e5c25d97a22bf5bd4d8cbac5756b17072
Ankit Konchady <ankit.konchady@neebal.com>
2026-03-26 11:35:16 +0530

    [ADD]--added LinkedListImpl

---
+++ src/practice/basic/LinkedListImpl.java
@@ -0,0 +1,58 @@
+package practice.basic;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
+public class LinkedListImpl {
+
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static LinkedListImpl insert(LinkedListImpl list, int data) {
+        Node newNode = new Node(data);
+
+        if (list.head == null) {
+            list.head = newNode;
+        } else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+        return list;
+    }
+
+    public static void printList(LinkedListImpl list){
+        Node currNode = list.head;
+        System.out.println("LinkedListImpl : ");
+        while(currNode!=null){
+            System.out.println(currNode.data + "");
+            currNode=currNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListImpl list = new LinkedListImpl();
+
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+        list = insert(list, 6);
+        list = insert(list, 7);
+        list = insert(list, 8);
+
+        printList(list);
+    }
+}
