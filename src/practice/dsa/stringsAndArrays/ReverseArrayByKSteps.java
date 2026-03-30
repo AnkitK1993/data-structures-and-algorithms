5379da5143ae6efbadc7a8deca4dededdb3c1962
AnkitK1993 <ankit.konchady@gmail.com>
2026-03-30 11:01:43 +0530

    [ADD]-added 189

---
+++ src/practice/dsa/stringsAndArrays/ReverseArrayByKSteps.java
@@ -0,0 +1,40 @@
+package practice.dsa.stringsAndArrays;
+
+public class ReverseArrayByKSteps {
+    private int[] nums;
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        for(int i:arr)
+            System.out.print(i);
+        System.out.println();
+        int k = 3;
+        ReverseArrayByKSteps obj = new ReverseArrayByKSteps();
+        obj.rotate(arr, k);
+    }
+
+    public void rotate(int[] nums, int k) {
+        this.nums = nums;
+
+        int n = nums.length;
+        k = k % n;
+
+        reverse(0, n - 1);
+        reverse(0, k - 1);
+        reverse(k, n - 1);
+    }
+
+    private void reverse(int i, int j) {
+        while (i < j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                i++;
+                j--;
+        }
+        for (int k : nums)
+            System.out.print(k);
+        System.out.println();
+    }
+}
